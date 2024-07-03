c0781542cb782c34f60de6a5a45e88ed3e3157df
Inamov Asilbek <playerallgames007@gmail.cpm>
2024-07-03 23:03:41 +0500

    Fix

--- index.js
+++ index.js
@@ -7,45 +7,45 @@ const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+})
 
 
-    bot.onText(/\/start/, (msg) => {
-        // console.log(msg);
-        const opt = {
-            reply_to_message_id: msg.message_id,
-            reply_markup: JSON.stringify({
-                keyboard: [
-                    ["Ahvoling qanday?"],
-                    ["Ishlaring joydami?"],
-                    ["Isming nima?"],
-                    ["Bugungi sana?"]
-                ],
-                "one_time_keyboard": true
-            })
-        }
-        if (msg.chat.type === "private") {
-            bot.sendMessage(msg.chat.id, "Salom Men bot man", opt)
-        }
-    })
-    bot.onText(/Ahvoling qanday ?/, (msg) => {
-        setTimeout(() => {
-            bot.sendMessage(msg.chat.id, " Zo'r ヾ(•ω•`)o")
-        }, [2000])
-    })
-    bot.onText(/Ishlaring joydami ?/, (msg) => {
-        setTimeout(() => {
-            bot.sendMessage(msg.chat.id, " Yaxshi \(￣︶￣*\))")
-        }, [2000])
-    })
-    bot.onText(/Isming nima ?/, (msg) => {
-        setTimeout(() => {
-            bot.sendMessage(msg.chat.id, " Bot （＾∀＾●）ﾉｼ")
-        }, [2000])
-    })
-    bot.onText(/Bugungi sana ?/, (msg) => {
-        const date = new Date();
-        setTimeout(() => {
-            bot.sendMessage(msg.chat.id, " Kalendarga qara " + date.getDate().toString() + " o(*°▽°*)o")
-        }, [1000])
-    })
+bot.onText(/\/start/, (msg) => {
+    // console.log(msg);
+    const opt = {
+        reply_to_message_id: msg.message_id,
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ["Ahvoling qanday?"],
+                ["Ishlaring joydami?"],
+                ["Isming nima?"],
+                ["Bugungi sana?"]
+            ],
+            "one_time_keyboard": true
+        })
+    }
+    if (msg.chat.type === "private") {
+        bot.sendMessage(msg.chat.id, "Salom Men bot man", opt)
+    }
+})
+bot.onText(/Ahvoling qanday ?/, (msg) => {
+    setTimeout(() => {
+        bot.sendMessage(msg.chat.id, " Zo'r ヾ(•ω•`)o")
+    }, [2000])
+})
+bot.onText(/Ishlaring joydami ?/, (msg) => {
+    setTimeout(() => {
+        bot.sendMessage(msg.chat.id, " Yaxshi \(￣︶￣*\))")
+    }, [2000])
+})
+bot.onText(/Isming nima ?/, (msg) => {
+    setTimeout(() => {
+        bot.sendMessage(msg.chat.id, " Bot （＾∀＾●）ﾉｼ")
+    }, [2000])
+})
+bot.onText(/Bugungi sana ?/, (msg) => {
+    const date = new Date();
+    setTimeout(() => {
+        bot.sendMessage(msg.chat.id, " Kalendarga qara " + date.getDate().toString() + " o(*°▽°*)o")
+    }, [1000])
 })
